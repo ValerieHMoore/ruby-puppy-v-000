a4a92ef7c78c4301a5f5bb9c2b48de9799ded7cd
Valerie Moore <periwinkle17@outlook.com>
2018-06-23 23:15:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,17 @@
 class Dog
-
-
-
-
-
+ 
+  @@all = []
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+ 
+  def self.all
+    @@all.each do |song|
+      puts song.name
+    end
+  end
 end
